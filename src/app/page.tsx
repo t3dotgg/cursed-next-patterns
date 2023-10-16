e638da8919c24b8deb999a962bdfa4fdc484c182
Theo Browne <me@t3.gg>
2023-10-15 17:06:35 -0700

    last minute cleanup with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex gap-4 flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-bold">Cursed Next Patterns</h1>
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">Cursed Next Patterns</h1>
+        <a
+          className="text-blue-300 underline"
+          href="https://cursed-next-patterns.t3.gg"
+        >
+          cursed-next-patterns.t3.gg
+        </a>
+      </div>
       <div>Click one of the links below to hurt your brain</div>
       <ul className="list-disc text-xl space-y-4">
         <li>
@@ -22,6 +30,17 @@ export default function Home() {
           </Link>
         </li>
       </ul>
+
+      <div className="text-center mt-8">
+        Also, check out this repo on GitHub:
+        <br />
+        <a
+          href="https://github.com/t3dotgg/cursed-next-patterns"
+          className="text-blue-300 underline"
+        >
+          https://github.com/t3dotgg/cursed-next-patterns
+        </a>
+      </div>
     </main>
   );
 }
